e8b8c4b712d5e669e5520cebe88468c8983306b8
SarikViper <dzamolovsardor5@gmail.com>
2025-03-29 22:24:59 +0500

    fixed

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -5,14 +5,14 @@ const Auth = () => {
 
     return (
         <div className="h-screen w-full bg-[#121212] flex items-center justify-center">
-            <div className="flex flex-col items-center justify-center w-full max-w-[400px] lg:max-w-[600px]">
+            <div className="flex flex-col items-center justify-center w-full max-w-[450px] lg:max-w-[600px]">
                 <div className="bg-[#121212] h-[465px] lg:h-[23px] w-full bg-cover"></div>
                 <div className="bg-[#121212] w-full h-[449px] flex flex-col items-center pb-20 gap-5 justify-center"> 
-                    <div className="flex flex-col text-white items-center">
+                    <div className="flex flex-col w-full text-white items-center">
                         <img className="w-12 h-12" src="/logoAuth.png" alt="Spotify Logo" />
                         <p className="text-2xl font-bold text-center">Millions of Songs. Free on Spotify.</p>
                     </div>
-                    <div className="text-white flex flex-col gap-5 pb-20 items-center">
+                    <div className="text-white w-full flex flex-col gap-5 items-center">
                         <Button className={`${buttonStyles} bg-green-400 text-black hover:text-white hover:bg-green-900`}>
                             Sign up free
                         </Button>
