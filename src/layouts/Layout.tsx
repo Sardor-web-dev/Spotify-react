4b83ae84e241a64ad883ec3bf34091c9e6ac35fb
SarikViper <dzamolovsardor5@gmail.com>
2025-03-30 21:42:00 +0500

    ready header

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/custom/Header";
+import Footer from "../components/custom/Footer";
 
 const Layout = () => {
     return (
