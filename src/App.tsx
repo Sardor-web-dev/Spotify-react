190ee85e16a4853dd8fa49489d8c9c87fffbcc85
SarikViper <dzamolovsardor5@gmail.com>
2025-03-31 13:13:32 +0500

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Auth from "./pages/Auth";
 import Home from "./pages/Home";
-import Header from "./components/custom/Header";
-import Player from "./components/custom/Player";
 import Layout from "./layouts/Layout";
 
 function App() {
