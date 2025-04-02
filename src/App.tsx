1449c99f7b38c86f843dc79ceae825dbd379fcc7
SarikViper <dzamolovsardor5@gmail.com>
2025-04-02 18:27:34 +0500

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes, useNavigate} from "react-router-dom";
 import Auth from "./pages/Auth";
 import Layout from "./layouts/Layout";
 
 function App() {
     const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
-
     return (
         <Routes>
             <Route path="/" element={isAuthenticated ? <Layout /> : <Navigate to="/login" />} >
                 <Route index element={<Layout />} />
             </Route>
 
-            <Route path="/me" element={<layout />}>
+            <Route path="/me" element={<Layout />}>
             </Route>
 
             <Route path="/login" element={<Auth />} />
